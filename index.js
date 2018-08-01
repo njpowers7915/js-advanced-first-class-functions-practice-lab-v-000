2ea0ca678d88073eb83fbb94917fb1a1a09a8fa5
Nicholas Powers <njpowers7915@gmail.com>
2018-08-01 14:08:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const logDriverNames = function(array) {
+  for (const driver of array) {
+    console.log(driver.name);
+  }
+}
+
+const logDriversByHometown = function(array, location) {
+  for (const driver of array) {
+    if (driver.hometown == location) {
+      console.log(driver.name);
+    }
+  }
+}
+
+const driversByRevenue = function(array) {
+  const arrayCopy = [...array];
+  return arrayCopy.sort(function (a, b) {
+    return a.revenue - b.revenue;
+  });
+}
+
+const driversByName = function(array) {
+  const arrayCopy = [...array];
+  return arrayCopy.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+}
